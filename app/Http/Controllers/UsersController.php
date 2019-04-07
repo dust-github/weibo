70ad78a047b831f7043294c9fa6cfbfe67824015
Dust <1036605035@qq.com>
2019-04-07 11:35:05 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -12,6 +12,15 @@ class UsersController extends Controller
         return view('users.create');
     }
 
+    public function store(Request $request)
+    {
+    	$this->validate($request, [
+    		'name' => 'required|max:50',
+    		'email' => 'required|email|unique:users|max:255',
+    		'password' => 'required|confirmed|min:6'
+    	]);
+    }
+
     public function show(User $user)
     {
     	return view('users.show', compact('user'));
