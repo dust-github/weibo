b1ffe4ebb6f6e9840bf940aff474459d39df0bc6
Dust <1036605035@qq.com>
2019-04-08 14:24:13 +0000

    登录成功后导航的逻辑

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,16 +2,19 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 新手入门教程</title>
-	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  	<link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
-	@include('layouts._header')
-
+  	@include('layouts._header')
     <div class="container">
+      <div class="offset-md-1 col-md-10">
         @include('share._messages')
-		@yield('content')
-		@include('layouts._footer')
+    		@yield('content')
+    		@include('layouts._footer')
+      </div>
     </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
   </body>
 </html>
 
