a5f76a4ec09981e38746718154762d450115bb18
Dust <1036605035@qq.com>
2019-04-09 02:14:33 +0000

    管理员可删除用户

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'dust';
         $user->email = '1036605035@qq.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
