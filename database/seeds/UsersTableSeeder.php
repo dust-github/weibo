4e2bac03270a92065523b95e074b08e19d145522
Dust <1036605035@qq.com>
2019-04-09 09:23:34 +0000

    用户微博列表、

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'dust';
         $user->email = '1036605035@qq.com';
+        $user->password = bcrypt('123456');
         $user->is_admin = true;
         $user->save();
     }
