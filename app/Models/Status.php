e1867136040f2b33a6370a77984d00bae6921896
Dust <1036605035@qq.com>
2019-04-10 14:55:11 +0000

    关注用户动态刘

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -8,6 +8,8 @@ class Status extends Model
 {
 	protected $fillable = ['content'];
 
+	static $test  = 111;
+
     public function user()
     {
         return $this->belongsTo(User::class);
--- app/Models/User.php
+++ app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
@@ -46,18 +47,21 @@ class User extends Authenticatable
 
     public function feed()
     {
-        return $this->statuses()
-                    ->orderBy('created_at', 'desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+                        ->with('user')
+                        ->orderBy('created_at', 'desc');
     }
 
     public function followers()
     {
-        return $this->belongsToMany(User::Class, 'followers', 'user_id', 'follower_id');
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
     }
 
     public function followings()
     {
-        return $this->belongsToMany(User::Class, 'followers', 'follower_id', 'user_id');
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
     }
 
     public function follow($user_ids)
